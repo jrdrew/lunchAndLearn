cbdb07e20e238ebd90e986ed1d6c4249431b4a45
Jon Drew <jrdrew@gmail.com>
2016-04-13 16:51:18 -0400

    adding an initial dummy class and test to make sure things are happy

---
+++ src/main/java/org/jrdrew/shortener/Canary.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2014-2016 Cazena, Inc., as an unpublished work.
+ * This notice does not imply unrestricted or public access to these
+ * materials which are a trade secret of Cazena, Inc. or its
+ * subsidiaries or affiliates (together referred to as "Cazena"), and
+ * which may not be copied, reproduced, used, sold or transferred to any
+ * third party without Cazena's prior written consent.
+ * <p/>
+ * All rights reserved.
+ * Created by jondrew on 4/13/16.
+ */
+package org.jrdrew.shortener;
+
+public class Canary {
+}
